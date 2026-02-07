49085dc4c3e6ee41e1f352936e1e2a8dd405a5fc
yakobos <yakobos@proton.me>
2026-02-06 16:58:28 -0800

    deployed and works

--- phase3/main.tf
+++ phase3/main.tf
@@ -51,55 +51,60 @@ resource "azurerm_kubernetes_cluster" "main" {
 
 }
 
+output "kubectl" {
+  value = "az aks get-credentials --name ${azurerm_kubernetes_cluster.main.name} --resource-group ${azurerm_resource_group.aks.name} --admin --overwrite"
+}
+
 # DB
 
-#resource "azurerm_postgresql_flexible_server" "n8n_db" {
-#
-#  name                = "psql-n8n-mercury"
-#  resource_group_name = azurerm_resource_group.aks.name
-#  location            = azurerm_resource_group.aks.location
-#  zone                = "2"
-#
-#  administrator_login    = "n8nadmin"
-#  administrator_password = "n8n-password-123"
-#
-#  sku_name   = "B_Standard_B1ms"
-#  storage_mb = 32768
-#  version    = "16"
-#
-#  backup_retention_days = 7
-#
-#  # Allow Azure services to access (needed for AKS)
-#  public_network_access_enabled = true
-#}
-#
-#resource "azurerm_postgresql_flexible_server_configuration" "disable_ssl" {
-#  name      = "require_secure_transport"
-#  server_id = azurerm_postgresql_flexible_server.n8n_db.id
-#  value     = "OFF"
-#}
-#
-#resource "azurerm_postgresql_flexible_server_database" "n8n" {
-#  name      = "n8n"
-#  server_id = azurerm_postgresql_flexible_server.n8n_db.id
-#}
-#
-#resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
-#  name             = "AllowAzureServices"
-#  server_id        = azurerm_postgresql_flexible_server.n8n_db.id
-#  start_ip_address = "0.0.0.0"
-#  end_ip_address   = "0.0.0.0"
-#}
-#
-#output "db_host" {
-#  value = azurerm_postgresql_flexible_server.n8n_db.fqdn
-#}
-#
-#output "db_name" {
-#  value = azurerm_postgresql_flexible_server_database.n8n.name
-#}
-#
-#output "db_user" {
-#  value = azurerm_postgresql_flexible_server.n8n_db.administrator_login
-#}
-#
+resource "azurerm_postgresql_flexible_server" "n8n_db" {
+
+
+  name                = "psql-n8n-${var.name}"
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = azurerm_resource_group.aks.location
+  zone                = "2"
+
+  administrator_login    = "n8nadmin"
+  administrator_password = "n8n-password-123"
+
+  sku_name   = "B_Standard_B1ms"
+  storage_mb = 32768
+  version    = "16"
+
+  backup_retention_days = 7
+
+  # Allow Azure services to access (needed for AKS)
+  public_network_access_enabled = true
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "disable_ssl" {
+  name      = "require_secure_transport"
+  server_id = azurerm_postgresql_flexible_server.n8n_db.id
+  value     = "OFF"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "n8n" {
+  name      = "n8n"
+  server_id = azurerm_postgresql_flexible_server.n8n_db.id
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_postgresql_flexible_server.n8n_db.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+output "db_host" {
+  value = azurerm_postgresql_flexible_server.n8n_db.fqdn
+}
+
+output "db_name" {
+  value = azurerm_postgresql_flexible_server_database.n8n.name
+}
+
+output "db_user" {
+  value = azurerm_postgresql_flexible_server.n8n_db.administrator_login
+}
+
